d92e15c31e74d5031f8102f3dacc65076ab77bc7
seph <seph@directionless.org>
2012-07-10 13:03:20 -0400

    First pass at rkhunter

---
+++ recipes/default.rb
@@ -0,0 +1,36 @@
+# rkhunter takes some of it's configs from dpkg, so we need to handle
+# it's response file.
+#template "/etc/dpkg/rkhunter.response" do
+#  source "debian.response.erb"
+#  mode 0644
+#  owner "root"
+#  group "root"
+#end
+
+package "rkhunter" do
+  action :install
+#  response_file "/etc/dpkg/rkhunter.response"
+end
+
+template "/etc/default/rkhunter" do
+  source "debian-default.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
+
+template "/etc/rkhunter.conf" do
+  source "rkhunter.conf.erb"
+  mode 0644
+  owner "root"
+  group "root"
+end
+
+case value
+when NilClass
+  # skip
+when Array
+  value.join(" ")
+else
+  value.to_s
+end
