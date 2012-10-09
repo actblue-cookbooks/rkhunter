a8fcd172562a52bd291ffb7ce5b8ae8997bf7e2b
seph <seph@directionless.org>
2012-10-09 11:35:28 -0400

    rkhunter fix filename

--- recipes/default.rb
+++ recipes/default.rb
@@ -26,7 +26,7 @@ template "/etc/rkhunter.conf.local" do
   notifies :run, "execute[rkhunter update]", :delayed
 end
 
-template " /etc/apt/apt.conf.d/90rkhunter" do
+template "/etc/apt/apt.conf.d/90rkhunter" do
   source "rkhunter.apt.erb"
   mode 0644
   owner "root"
